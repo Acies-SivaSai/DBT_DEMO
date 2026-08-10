028a13fa0938fe998abd7e82896bcdbb2d7edb79
sivasaireddy0404@gmail.com <sivasaireddy0404@gmail.com>
2026-08-10 10:27:15 +0000

    date_dimension file got created

---
+++ models/date_dimension.sql
@@ -0,0 +1,28 @@
+with cte as (
+    select
+        try_to_timestamp(STARTED_AT) as STARTED_AT,
+        DATE(try_to_timestamp(STARTED_AT)) as DATE_STARTED_AT,
+        HOUR(try_to_timestamp(STARTED_AT)) AS HOUR_STARTED_AT,
+        
+        case 
+        when dayname(try_to_timestamp(STARTED_AT)) in ('Sat','Sun')
+        then 'WEEKEND'
+        else 'BUSINESSDAY'
+        end as day_type,
+
+        case
+        when month(try_to_timestamp(STARTED_AT)) in (12,1,2)
+        then 'WINTER'
+        when month(try_to_timestamp(STARTED_AT)) in (3,4,5)
+        then 'SPRING'
+        when month(try_to_timestamp(STARTED_AT)) in (6,7,8)
+        then 'SUMMER'
+        ELSE 'AUTUMN'
+        end as station_of_year
+        
+    from {{ source('demo', 'bikes') }}
+    where STARTED_AT != 'started_at'
+)
+
+select *
+from cte
